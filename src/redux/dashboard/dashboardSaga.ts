f9b1b9977210900127b36611f886ae5d1c6b616a
medev21 <benmarteen07@gmail.com>
2022-04-18 18:14:09 -0400

    import redux saga effect put

--- src/redux/dashboard/dashboardSaga.ts
+++ src/redux/dashboard/dashboardSaga.ts
@@ -1,4 +1,4 @@
-import { call, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import { fetchApi } from '../../client_services/fetchAPI';
 import { DashboardTypes } from './actionTypes';
 
