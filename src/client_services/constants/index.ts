7d0f8bf883b6bebd6f3adc65613be242e57be9c4
medev21 <benmarteen07@gmail.com>
2022-05-04 23:12:13 -0400

    add constants in client services

---
+++ src/client_services/constants/index.ts
@@ -0,0 +1,7 @@
+export const baseURL: string = 'https://api.coingecko.com';
+
+export const APIVersion: string = '/api/v3';
+
+export const API_PATHS = {
+    COINS_BY_MARKET: `${APIVersion}/coins/markets`,
+};
