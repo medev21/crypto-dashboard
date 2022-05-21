619bd612d7a4452dd4c1a894e16c8418175c7481
medev21 <benmarteen07@gmail.com>
2022-05-20 20:27:32 -0400

    add favorites sagas to saga index

--- src/reduxSetup/sagas/index.ts
+++ src/reduxSetup/sagas/index.ts
@@ -3,7 +3,11 @@ import { all, fork } from 'redux-saga/effects';
 
 // sagas
 import * as dashboardSaga from 'reduxSetup/dashboard/dashboardSaga';
+import * as favortiesSaga from 'reduxSetup/favorites/favoritesSaga';
 
 export default function* rootSaga() {
-    yield all([...Object.values(dashboardSaga).map(fork)]);
+    yield all([
+        ...Object.values(dashboardSaga).map(fork),
+        // ...Object.values(favortiesSaga).map(fork),
+    ]);
 }
